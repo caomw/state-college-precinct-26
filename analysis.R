b5862e08a908de922a52a4386d950283e053f870
rogilmore <thatrickgilmore@gmail.com>
2014-11-05 11:39:22 -0500

    cleaned up plotting & printing

--- analysis.R
+++ analysis.R
@@ -7,9 +7,7 @@ p1 = qplot( x=year, y=cast,
             data=precinct.26, 
             color=type, 
             geom=c("point", "line")
-            )
-
-p1 + labs(title="Votes Cast By Year and Election Type -- Precinct 26: State College East 3")
+            ) + labs(title="Votes Cast By Year and Election Type -- Precinct 26: State College East 3")
 ggsave(filename="votes-cast.png", plot=p1)
 
 # Turnout
@@ -17,8 +15,7 @@ p2 = qplot( x=year, y=cast/registered,
               data=precinct.26, 
               color=type, 
               geom=c("point", "line")
-)
-p2 + labs(title="Turnout Proportion By Year and Election Type -- Precinct 26: State College East 3")
+) + labs(title="Turnout Proportion By Year and Election Type -- Precinct 26: State College East 3")
 ggsave(filename="turnout-p.png", plot=p2)
 
 # Registered
@@ -26,8 +23,5 @@ p3 = qplot( x=year, y=registered,
             data=precinct.26, 
             color=type, 
             geom=c("point", "line")
-)
-p3 + labs(title="Registered Voters By Year and Election Type -- Precinct 26: State College East 3")
+) + labs(title="Registered Voters By Year and Election Type -- Precinct 26: State College East 3")
 ggsave(filename="registered.png", plot=p3)
-
-
